f575ad3877ae7e9d11534bef3cc312516d51becd
Mathman <17e23052@marling.gloucs.sch.uk>
2020-09-29 09:39:56 +0000

    Binary and hexadecimal v1.0

---
+++ main.py
@@ -0,0 +1,11 @@
+#This program converts a number entered into its binary and hexadecimal equivalents.
+from time import sleep
+print("Enter a number between 0 and 255:")
+number = int(input())
+if number >= 256 or number < 0:
+  print("Please enter a number between 0 or 255")
+  sleep(999)
+binary = bin(number)
+print(f"The binary of this number is {binary}")
+hexa = hex(number)
+print(f"The hexadecimal of this number is {hexa}")
